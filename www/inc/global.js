2e7dac2c761d29a58f343519ee77baa07f874472
Sergej Chodarev <sergejx@centrum.sk>
2014-05-17 17:17:07 +0200

    Switch back to CSS based image resizing
    It seems to work better after all

--- www/inc/global.js
+++ www/inc/global.js
@@ -7,22 +7,3 @@ function toggle_div(classname) {
 		}
 }
 
-/* Resize image according to container size */
-function resizeImage() {
-    var width = $("#preview").width() - 22;
-    var margin = 0;
-    var image = $("#preview img");
-    if (width > image.attr('width')) {
-        margin = (width - image.attr('width')) / 2
-        width = image.attr('width');
-    }
-    $("#preview img").width(width);
-    $("#preview img").css('margin-left', margin);
-}
-
-$(function() {
-    if ($("#preview").length > 0) {
-        resizeImage();
-        $(window).resize(resizeImage);
-    }
-});
